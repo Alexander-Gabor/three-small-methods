5918986573903029dc0cd7af10a9088244d60341
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-09-14 21:57:30 +0200

    Adding logic to check for length of personalNumber

--- src/main/java/ThreeSmallMethods.java
+++ src/main/java/ThreeSmallMethods.java
@@ -5,14 +5,18 @@ public class ThreeSmallMethods {
     }
 
     public boolean hasLegalGenderManInSweden(String personalNumber){
-        char secondToLastDigit = personalNumber.charAt(personalNumber.length() - 2);
-        return Character.getNumericValue(secondToLastDigit) % 2 != 0;
+        if (personalNumber.length() == 10) {
+            char secondToLastDigit = personalNumber.charAt(personalNumber.length() - 2);
+            return Character.getNumericValue(secondToLastDigit) % 2 != 0;
+        } else{
+            return false;
+        }
     }
 
     public int firstTwoDigitInteger(int[] numbers) {
         for (int i = 0; i < numbers.length; i++) {
             int number = numbers[i];
-            if (number>=10 && number<100) {
+            if (Math.abs(number) >= 10 && Math.abs(number) < 100) {
                 return number;
             }
         }
