b8c3c50f2a9518b6628ee59b0cdc7cb8fd12193b
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-09-14 18:38:53 +0200

    Implement initial logic in hasLegalGenderManInSweden method

--- src/main/java/ThreeSmallMethods.java
+++ src/main/java/ThreeSmallMethods.java
@@ -5,12 +5,14 @@ public class ThreeSmallMethods {
     }
 
     public boolean hasLegalGenderManInSweden(String personalNumber){
-
+        char secondToLastDigit = personalNumber.charAt(personalNumber.length() - 2);
+        return Character.getNumericValue(secondToLastDigit) % 2 != 0;
     }
 
     public int firstTwoDigitInteger(int[] numbers) {
-        for (int number : numbers) {
-            if (number >= 10 && number < 100) {
+        for (int i = 0; i < numbers.length; i++) {
+            int number = numbers[i];
+            if (number>=10 && number<100) {
                 return number;
             }
         }
