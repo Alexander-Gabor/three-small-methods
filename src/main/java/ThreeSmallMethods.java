5cbcfeaccd5192b456efc3f5c879b2387fc661df
AlexanderGabor <gaboralexanderalex@gmail.com>
2024-09-14 18:23:59 +0200

    Implement logic for isYoung method

--- src/main/java/ThreeSmallMethods.java
+++ src/main/java/ThreeSmallMethods.java
@@ -1,14 +1,20 @@
 public class ThreeSmallMethods {
 
     public boolean isYoung(int age) {
-        return false;
+        return age <= 30;
     }
 
     public boolean hasLegalGenderManInSweden(String personalNumber){
-        return false;
+
     }
 
     public int firstTwoDigitInteger(int[] numbers) {
+        for (int i = 0; i < numbers.length; i++) {
+            int number = numbers[i];
+            if (number>=10 && number<100) {
+                return number;
+            }
+        }
         return -1;
     }
 }
